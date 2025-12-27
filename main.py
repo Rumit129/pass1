13807d996ac0c2c07a7e29dca5bbc266af653226
bambarbino <bambarbino10@gmail.com>
2025-12-27 17:25:15 +0300

    func1 and func2 commit

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 def main():
     print('Start')
+    func1()
     func2()
     print('End')
 
 
 def func2():
     print(2)
+
+def func1():
+    print('1')
 
 if __name__ == '__main__':
     main()
